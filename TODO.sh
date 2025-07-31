6bc8be041efd14e57b43a7c942f5dab9c09f31a9
gjpin <3874515+gjpin@users.noreply.github.com>
2025-07-31 11:00:18 +0100

    update todo

--- TODO.sh
+++ TODO.sh
@@ -2,3 +2,20 @@ flatpak overrides
 - [com.ranfdev.DistroShelf](https://github.com/flathub/com.ranfdev.DistroShelf/blob/master/com.ranfdev.DistroShelf.json)
 - [LACT](https://github.com/flathub/io.github.ilya_zlobintsev.LACT/blob/master/io.github.ilya_zlobintsev.LACT.yaml)
 - [virt-manager](https://github.com/flathub/org.virt_manager.virt-manager/blob/master/org.virt_manager.virt-manager.yaml)
+
+# Virtual displays
+# https://www.answeroverflow.com/m/1276584307477057640
+# https://www.reddit.com/r/linux_gaming/comments/199ylqz/streaming_with_sunshine_from_virtual_screens/
+# https://git.linuxtv.org/v4l-utils.git/tree/utils/edid-decode/data
+# https://people.freedesktop.org/~imirkin/edid-decode/
+# https://docs.fedoraproject.org/en-US/fedora-silverblue/technical-information/#filesystem-layout
+# https://www.reddit.com/r/linux_gaming/comments/1h2o0re/comment/mtq730l/
+# https://gist.github.com/MrHighVoltage/78ca58218a569d253433fd4be883c6c3
+# Displays with 4k60 + HDR + 1280x800 support
+# Display port: dell-up2718q-dp
+# HDMI: samsung-q800t-hdmi2.1-game
+
+# wake from sleep
+# https://www.reddit.com/r/Bazzite/comments/1h95rua/wake_from_sleep_with_8bitdo/
+# https://www.answeroverflow.com/m/1320873947344015523
+# https://askubuntu.com/questions/848698/wake-up-from-suspend-using-usb-device
