87daeabb833c2ef1ec95b98eb5630688dcb6a8c8
gjpin <3874515+gjpin@users.noreply.github.com>
2025-08-01 19:48:47 +0100

    ludusavi config

--- setup.sh
+++ setup.sh
@@ -192,6 +192,10 @@ brew services start syncthing
 flatpak install -y flathub com.github.mtkennerly.ludusavi
 curl https://raw.githubusercontent.com/gjpin/bazzite/main/configs/flatpak/com.github.mtkennerly.ludusavi -o ${HOME}/.local/share/flatpak/overrides/com.github.mtkennerly.ludusavi
 
+# Import Ludusavi config
+mkdir -p ${HOME}/.var/app/com.github.mtkennerly.ludusavi/config/ludusavi
+curl https://raw.githubusercontent.com/gjpin/bazzite/main/configs/ludusavi/config.yaml | envsubst > ${HOME}/.var/app/com.github.mtkennerly.ludusavi/config/ludusavi/config.yaml
+
 # Set automatic backups
 # https://github.com/mtkennerly/ludusavi/blob/master/docs/help/backup-automation.md
 tee ~/.config/systemd/user/ludusavi-backup.service << 'EOF'
