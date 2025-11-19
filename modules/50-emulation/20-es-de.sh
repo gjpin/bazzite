7427b767cb66233b2d5229e77203333c6aa45316
gjpin <3874515+gjpin@users.noreply.github.com>
2025-11-19 15:51:12 +0000

    es-de fix repo

--- modules/50-emulation/20-es-de.sh
+++ modules/50-emulation/20-es-de.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/bash
 
 # Instal ES-DE
-sudo flatpak install -y flathub org.es.ES-DE
+sudo flatpak install -y private org.es.ES-DE
 
 # Import Flatpak overrides
 cp ./configs/flatpak/org.es.ES-DE ${HOME}/.local/share/flatpak/overrides/org.es.ES-DE
