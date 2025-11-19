ed5c0f0af5a2d8be353b56e25b646bdaf7209073
gjpin <3874515+gjpin@users.noreply.github.com>
2025-11-19 15:48:57 +0000

    update flatpaks

--- modules/40-applications/10-flatpak.sh
+++ modules/40-applications/10-flatpak.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/bash
 
 # Install Flatpak runtimes and extensions
-sudo flatpak install -y flathub org.freedesktop.Platform.ffmpeg-full//25.08
-sudo flatpak install -y flathub org.freedesktop.Platform.GStreamer.gstreamer-vaapi//25.08
+sudo flatpak install -y flathub org.freedesktop.Platform.codecs-extra//25.08
 sudo flatpak install -y flathub org.freedesktop.Platform.GL.default//25.08-extra
 sudo flatpak install -y flathub org.freedesktop.Platform.GL32.default//25.08-extra
 sudo flatpak install -y flathub org.freedesktop.Sdk//25.08
