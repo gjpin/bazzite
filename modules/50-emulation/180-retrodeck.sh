85ac438cf404fead9614920a7a491bafb2ba7d11
gjpin <3874515+gjpin@users.noreply.github.com>
2025-11-21 17:33:21 +0000

    fix retrodeck

--- modules/50-emulation/180-retrodeck.sh
+++ modules/50-emulation/180-retrodeck.sh
@@ -5,7 +5,7 @@ source lib/logging.sh
 log_start
 
 # Instal RetroDeck
-sudo flatpak install -y private net.retrodeck.retrodeck
+sudo flatpak install -y flathub net.retrodeck.retrodeck
 
 # Import Flatpak overrides
 cp ./configs/flatpak/net.retrodeck.retrodeck ${HOME}/.local/share/flatpak/overrides/net.retrodeck.retrodeck
